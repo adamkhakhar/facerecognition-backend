62435a91ac72e8d9c364d268011a28fe7410bfcf
Adam Khakhar <akhakhar@wharton.upenn.edu>
2019-07-22 22:27:27 -0700

    Update db for heroku

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-rectangular-69648',
-      user : 'akhileshkhakhar',
-      password : '',
-      database : 'facerecognition'
+      host : process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
